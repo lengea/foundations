4ed777c2f4879fcea8ba0cd8f3188e481771ccfa
lengea <43341984+lengea@users.noreply.github.com>
2020-02-19 12:05:36 +0100

    Bla
    Bla

--- cgi-bin/first-cgi.py
+++ cgi-bin/first-cgi.py
@@ -1,3 +1,29 @@
+#!usr/bin/env python3
+
+import cgi
+
+form = cgi.FieldStorage()
+username = form.getvalue("name")
+
+print(f"""
+	<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello!</title>
+  </head>
+  <body>
+
+  <h1>Hello {username}</h1>
+    
+  </body>
+  
+  </html>
+
+ 
+	""")
+
+'''
+
 name = input("What is your name?")
 
 print('Hello,' + ' ' + name + '!')
@@ -5,6 +31,9 @@ print('Hello,' + ' ' + name + '!')
 age = input("What is your age?")
 
 print('You are' + ' ' + age + ' ' + 'years old!')
+
+Print('Welcome to the platform!')
+'''
 
 
 
